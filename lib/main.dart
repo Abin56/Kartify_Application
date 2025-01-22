b66dbbe942e109e27e4a575928e5e84a02db130f
rachna-jk <rachnajk12@gmail.com>
2025-01-22 14:19:55 +0530

    workout 22 Jan

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:kartify_applicaion/rachna.dart';
+import 'package:kartify_applicaion/sample.dart';
+import 'package:kartify_applicaion/screen.dart';
 
-void main(){
-
-print("hello");
+void main() {
+  print("hello");
   runApp(MyApp());
-
-
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  ClassRachana(),
+      home: ClassRachana(),
     );
   }
 }
--- lib/rachna.dart
+++ lib/rachna.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kartify_applicaion/widgets/s.dart';
 
 class ClassRachana extends StatelessWidget {
   const ClassRachana({super.key});
@@ -6,40 +9,18 @@ class ClassRachana extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            color: Colors.red,
-            height: 100,
-            width: 100,
-            child: Container(),
-          ),
-          Container(
-            color: Colors.yellow,
-            height: 100,
-            width: 100,
-            child: Container(),
-          ),
-          Container(
-            color: Colors.black,
-            height: 100,
-            width: 100,
-            child: Container(),
-          ),
-          Container(
-            color: Colors.amber,
-            height: 100,
-            width: 100,
-            child: Container(),
-          ),
-          Container(
-            color: Colors.red,
-            height: 100,
-            width: 100,
-            child: Container(),
-          ),
-        ],
-      ),
-    );
+        body: Center(
+      child: Container(
+          height: 400,
+          width: double.infinity,
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Column(
+            children: [
+              LoginContainer(),
+              LoginContainer(),
+              LoginContainer(),
+            ],
+          )),
+    ));
   }
 }
