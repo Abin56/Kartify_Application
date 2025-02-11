b6e84e6d86f5654c69706d74b82beb2bc511ef90
rachna-jk <rachnajk12@gmail.com>
2025-02-11 22:05:34 +0530

    f

--- lib/constants/sizes.dart
+++ lib/constants/sizes.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
-const h10sizedbox = SizedBox(height: 10,);
-const h20sizedbox = SizedBox(height: 20,);
+const h10sizedbox = SizedBox(
+  height: 10,
+);
+const h20sizedbox = SizedBox(
+  height: 20,
+);
+const h30sizedbox = SizedBox(height: 30);
+const h40sizedbox = SizedBox(height: 40);
+const h50sizedbox = SizedBox(height: 50);
 
-const w10sizedbox = SizedBox(width: 10,);
-const w20sizedbox = SizedBox(width: 20,);
+const w10sizedbox = SizedBox(
+  width: 10,
+);
+const w20sizedbox = SizedBox(
+  width: 20,
+);
+const w30sizedbox = SizedBox(
+  width: 30,
+);
+const w40sizedbox = SizedBox(width: 40);
+const w50sizedbox = SizedBox(width: 50);
+const w60sizebox = SizedBox(width: 60);
